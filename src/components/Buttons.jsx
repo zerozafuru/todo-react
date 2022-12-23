175fc5b53ba9a7d1a481f1e029f34897a6450bee
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-23 10:31:00 +0300

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,25 @@
-const Buttons = () => {
+const Buttons = (props) => {
+  const { todos, setTodos, filter, setFilter } = props;
 
+  const sortAll = () =>{
+setFilter(todos)
+  }
 
-  
+  const sortTodo = () =>{
+    const filtTodo = todos.filter(todo => todo.completed === false )
+    setFilter(filtTodo);
+  }
+
+  const sortDone = () =>{
+    const filtTodo = todos.filter(todo => todo.completed === true )
+    setFilter(filtTodo);
+  }
+
   return (
     <div className="btns">
-      <input type='button' value='all' />
-      <input type='button' value='todo' />
-      <input type='button' value='done' />
+      <input type='button' onClick={() => sortAll()} value='all' />
+      <input type='button' onClick={() => sortTodo()} value='todo' />
+      <input type='button' onClick={() => sortDone()} value='done' />
     </div>
   )
 }
