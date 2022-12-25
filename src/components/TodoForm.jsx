425e018f5adfbfcdcd6b79fa96e72e914a625698
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-26 00:22:36 +0300

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const TodoForm = (props) => {
-  const { todos, setTodos } = props;
+  const { todos, setTodos, filter, setFilter } = props;
   const [text, setText] = useState('');
   const saveTask = (e) => {
     e.preventDefault();
@@ -14,17 +14,23 @@ const TodoForm = (props) => {
       }
       setText("");
       setTodos([todo, ...todos]);
+      setFilter([todo, ...todos])
     }
   }
 
   const deleteAll = () => {
+
     setTodos([])
   }
-  
+
+  const completeAll = () => {
+    
+  }
+
 
   return (
     <form className="create-form" onSubmit={(e) => saveTask(e)}>
-      <input type='checkbox' />
+      <input type='checkbox' onChange={()=>completeAll()}/>
       <input className="create-input" autoFocus value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder="What needs to be done?" />
       <button type="button" onClick={() => deleteAll()}>&#10006;</button>
     </form>
