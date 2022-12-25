b4f4cc6e51613eb7d5a5bbced0644a53f57cff20
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-26 00:22:41 +0300

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,11 +1,11 @@
 import TaskItem from "./TaskItem";
 
 const TaskList = (props) => {
-  const { todos, setTodos, filter, setFilter } = props;
-  
+  const { todos, setTodos, filter, setFilter,sort, setSort } = props;
+
   return (
     filter.map((todo) => (
-      <TaskItem key={todo.id} todos={todos} todo={todo} setTodos={setTodos} />
+      <TaskItem key={todo.id} todos={todos} todo={todo} setTodos={setTodos} filter={filter} setFilter={setFilter} sort={sort} setSort={setSort} />
     ))
   )
 }
