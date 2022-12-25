7788e000fac366df727254dce968a7235b8d709a
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-26 00:22:50 +0300

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,25 +1,15 @@
+import sortAll from "../utills/sortAll";
+import sortTodo from "../utills/sortTodo";
+import sortDone from "../utills/sortDone";
+
 const Buttons = (props) => {
-  const { todos, setTodos, filter, setFilter } = props;
-
-  const sortAll = () =>{
-setFilter(todos)
-  }
-
-  const sortTodo = () =>{
-    const filtTodo = todos.filter(todo => todo.completed === false )
-    setFilter(filtTodo);
-  }
-
-  const sortDone = () =>{
-    const filtTodo = todos.filter(todo => todo.completed === true )
-    setFilter(filtTodo);
-  }
+  const { todos, setTodos, filter, setFilter, sort, setSort } = props;
 
   return (
     <div className="btns">
-      <input type='button' onClick={() => sortAll()} value='all' />
-      <input type='button' onClick={() => sortTodo()} value='todo' />
-      <input type='button' onClick={() => sortDone()} value='done' />
+      <input type='button' onClick={() => sortAll(setFilter, setSort,todos)} value='all' />
+      <input type='button' onClick={() => sortTodo(setFilter, setSort,todos)} value='todo' />
+      <input type='button' onClick={() => sortDone(setFilter, setSort,todos)} value='done' />
     </div>
   )
 }
